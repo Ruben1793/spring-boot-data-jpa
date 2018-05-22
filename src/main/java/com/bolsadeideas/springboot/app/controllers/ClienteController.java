bb0f605286ec94600c397059df43197b45a7de1d
Unknown <ruben.ojed9a3@gmail.com>
2018-05-21 22:46:15 -0500

    Agregando validaciones usando anotaciones

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -2,11 +2,15 @@ package com.bolsadeideas.springboot.app.controllers;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
@@ -33,7 +37,10 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value="/form", method = RequestMethod.POST)
-	public String guardar(Cliente cliente) {
+	public String guardar(@Valid Cliente cliente, BindingResult result) {
+		if(result.hasErrors()) {
+			return "form";
+		}
 		clienteDao.save(cliente);
 		return "redirect:/listar";
 	}
