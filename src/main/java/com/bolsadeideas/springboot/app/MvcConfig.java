f425cacf8b8664ecbb41841440f98802eecc662c
Unknown <ruben.ojed9a3@gmail.com>
2018-06-02 19:41:40 -0500

    Se corrigio problema al mostrar imagen

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -4,9 +4,11 @@ import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter{
 	
 	private final Logger log = LoggerFactory.getLogger(getClass()); 
