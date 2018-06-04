f7fb75a01a265d45e32562e4fafce1ab70501427
Administrator <ruben.ojed9a3@gmail.com>
2018-06-03 19:26:43 -0500

    Asociaciones: ManyToOne Bidireccional - Clases Entity Factura y Cliente

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -1,13 +1,18 @@
 package com.bolsadeideas.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -43,8 +48,14 @@ public class Cliente  implements Serializable{
 	@DateTimeFormat(pattern="yyy-MM-dd")
 	private Date createAt;
 	
+	@OneToMany(mappedBy="cliente" ,fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	private List<Factura> facturas;
+	
 	private String foto;
 	
+	public Cliente() {
+		facturas = new ArrayList<Factura>();
+	}
 	public Long getId() {
 		return id;
 	}
@@ -75,17 +86,19 @@ public class Cliente  implements Serializable{
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
-	@Override
-	public String toString() {
-		return "Cliente [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
-				+ ", createAt=" + createAt + "]";
-	}
-	
 	public String getFoto() {
 		return foto;
 	}
-
 	public void setFoto(String foto) {
 		this.foto = foto;
+	}
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+	public void addFactura(Factura factura) {
+		facturas.add(factura);
 	}
 }
