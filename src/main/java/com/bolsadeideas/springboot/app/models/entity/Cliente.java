455ba24905bb1cdb510c7767c88a4ef6eebae30c
Unknown <ruben.ojed9a3@gmail.com>
2018-06-09 15:34:35 -0500

    Listado de Facturas en la vista detalle de Cliente - ver.html

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -23,15 +23,15 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
-@Table(name="clientes")
-public class Cliente  implements Serializable{
+@Table(name = "clientes")
+public class Cliente implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotEmpty
 	private String nombre;
 	
@@ -41,64 +41,85 @@ public class Cliente  implements Serializable{
 	@NotEmpty
 	@Email
 	private String email;
-	
+
 	@NotNull
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern="yyy-MM-dd")
+	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
 	
-	@OneToMany(mappedBy="cliente" ,fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Factura> facturas;
-	
-	private String foto;
 	
 	public Cliente() {
 		facturas = new ArrayList<Factura>();
 	}
+
+	private String foto;
+	
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Date getCreateAt() {
 		return createAt;
 	}
+
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
+
 	public String getFoto() {
 		return foto;
 	}
+
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
+
 	public List<Factura> getFacturas() {
 		return facturas;
 	}
+
 	public void setFacturas(List<Factura> facturas) {
 		this.facturas = facturas;
 	}
+	
 	public void addFactura(Factura factura) {
 		facturas.add(factura);
 	}
+
+	@Override
+	public String toString() {
+		return nombre + " " + apellido;
+	}
+
 }
