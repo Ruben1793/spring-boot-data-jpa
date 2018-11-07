9033bb98092eab8662148b20e5a99205544cf366
Ruben <ruben.ojed9a3@gmail.com>
2018-11-06 20:26:00 -0600

    Añadiendo metodo configure HttpSecurity para reglas ACL en rutas

--- src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
@@ -17,5 +18,19 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		.withUser(users.username("admin").password("1234").roles("ADMIN","USER"))
 		.withUser(users.username("andres").password("12345").roles("USER"));
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/listar").permitAll()
+		.antMatchers("/ver/**").hasAnyRole("USER")
+		.antMatchers("/uploads/**").hasAnyRole("USER")
+		.antMatchers("/form/**").hasAnyRole("USER")
+		.antMatchers("/eliminar/**").hasAnyRole("ADMIN")
+		.antMatchers("/factura/**").hasAnyRole("ADMIN")
+		.antMatchers("/uploads/**").hasAnyRole("ADMIN")
+		.anyRequest().authenticated();
+	}
+	
+	
 
 }
