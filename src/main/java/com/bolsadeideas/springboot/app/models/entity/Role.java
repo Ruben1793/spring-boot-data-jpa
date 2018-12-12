41f0c8072bb6eaf684249a8bb3ea916ba42431c9
Unknown <ruben.ojed9a3@gmail.com>
2018-12-11 20:58:09 -0600

    Modificando entidad Role

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Role.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Role.java
@@ -7,9 +7,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(name="authorities")
+@Table(name="authorities", uniqueConstraints = {@UniqueConstraint(columnNames= {"user_id", "authority"})})
 public class Role implements Serializable{
 
 	private static final long serialVersionUID = 1L;
