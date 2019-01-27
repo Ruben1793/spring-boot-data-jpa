824e1a6b1919fa1312f1885dded2999ace9ccbf8
Ruben <ruben.ojed9a3@gmail.com>
2019-01-27 12:20:28 -0600

    usando @RestController

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -3,7 +3,6 @@ package com.bolsadeideas.springboot.app.controllers;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Collection;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -36,7 +35,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.multipart.MultipartFile;
@@ -46,7 +44,6 @@ import com.bolsadeideas.springboot.app.Service.IClienteService;
 import com.bolsadeideas.springboot.app.Service.IUploadServiceFile;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import com.bolsadeideas.springboot.app.util.paginator.PageRender;
-import com.bolsadeideas.springboot.app.view.xml.ClienteList;
 
 @Controller
 @SessionAttributes("cliente")
@@ -63,11 +60,6 @@ public class ClienteController {
 	@Autowired
 	private IUploadServiceFile uploadServiceFile;
 	
-	@GetMapping(value = "listar-rest")
-	public @ResponseBody ClienteList listarRest() {
-		return new ClienteList(clienteService.findAll());
-	}
-
 	@Secured("ROLE_USER")
 	@GetMapping(value = "/uploads/{filename:.+}")
 	public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
